d41b82a14ea3b1ef59819c3c547ecad7583df3cb
Francisco Tapia LH <francisco.tapia.t@gmail.com>
2022-09-05 19:33:45 -0300

    1° commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const path = require('path')
+
+module.exports = {
+  productionSourceMap: false,
+  outputDir: './lib',
+  publicPath: './',
+  chainWebpack:  (config) => {
+    config.resolve.alias.set('@', path.resolve(__dirname, 'src'));
+
+    if (process.env.NODE_ENV === 'production') {
+    }
+  },
+  configureWebpack: {
+    output: {
+      filename: '[name].js',
+      chunkFilename: '[name].js',
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      }
+    }
+  }
+}
